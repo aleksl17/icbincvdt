738cd842bd36846dadd23c7ef8903e283a040d7f
Haavard Friborg <haavfr17@uia.no>
2020-12-16 21:53:16 +0100

    gray mode css

---
+++ icbincvdt/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace icbincvdt.Pages
+{
+    public class About : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
