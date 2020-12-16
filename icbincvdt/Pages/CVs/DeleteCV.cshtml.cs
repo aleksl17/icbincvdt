084437bd648550438e021ad7b97682a305032da6
Aleksander Markus Lingstad <aleksl17@uia.no>
2020-12-16 21:03:54 +0100

    Added share CV page

---
+++ icbincvdt/Pages/CVs/DeleteCV.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using icbincvdt.Data;
+using icbincvdt.Models;
+
+namespace icbincvdt.Pages.CVs
+{
+    public class DeleteCVModel : PageModel
+    {
+        private readonly icbincvdt.Data.ApplicationDbContext _context;
+
+        public DeleteCVModel(icbincvdt.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public CV CV { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            CV = await _context.CVs
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.CVID == id);
+
+            if (CV == null)
+            {
+                return NotFound();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ErrorMessage = "Delete failed. Try again";
+            }
+            
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cv = await _context.CVs.FindAsync(id);
+
+            if (cv == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.CVs.Remove(cv);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            catch(DbUpdateException)
+            {
+                return RedirectToAction("./Delete", new {id, saveChangesError = true});
+            }
+        }
+    }
+}
